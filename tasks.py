33728083b10bdf3904854a6e1fa6b2efcebffe38
Jimmy Lin <64210965+j3ygithub@users.noreply.github.com>
2020-10-23 16:33:18 +0800

    Initial

---
+++ tasks.py
@@ -0,0 +1,43 @@
+from invoke import task
+from pathlib import Path
+import os
+import platform
+import json
+
+
+REPO_ROOT = Path(__file__).parent
+OS = platform.system().lower()
+VENV_DIRNAME = Path('.venv')
+PATHONPATHS = {
+    'windows': VENV_DIRNAME / 'Scripts' / 'python.exe',
+    'linux': VENV_DIRNAME / 'bin' / 'python',
+    'darwin': VENV_DIRNAME / 'bin' / 'python',
+}
+VSCODE_SETTINGS = {
+    "python.pythonPath": str(PATHONPATHS[OS]),
+    "python.linting.enabled": True,
+    "python.linting.pylintEnabled": False,
+    "python.linting.flake8Enabled": True,
+    "python.linting.flake8Args": ["--max-line-length=160"],
+    "python.formatting.provider": "autopep8",
+    "python.formatting.autopep8Args": ["--max-line-length=160"]
+}
+PATHONPATHS_ABS = {
+    'windows': REPO_ROOT / VENV_DIRNAME / 'Scripts' / 'python.exe',
+    'linux': REPO_ROOT.resolve() / VENV_DIRNAME / 'bin' / 'python',
+    'darwin': REPO_ROOT.resolve() / VENV_DIRNAME / 'bin' / 'python',
+}
+
+
+# VSCode
+
+@task
+def vscode(c):
+    s = json.dumps(VSCODE_SETTINGS, indent=4)
+    try:
+        os.mkdir(REPO_ROOT / '.vscode')
+    except FileExistsError:
+        pass
+    with open(REPO_ROOT / '.vscode' / 'settings.json', 'w', encoding='utf-8') as f:
+        f.write(s)
+    print('Done!')
