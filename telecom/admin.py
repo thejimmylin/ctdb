0f0ab2c97d691cd3aeab89abbfe6e00020a178c4
Jimmy Lin <64210965+j3ygithub@users.noreply.github.com>
2020-12-07 15:33:27 +0800

    Add Email model

--- telecom/admin.py
+++ telecom/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import Isp
+from .models import Email, Isp
 
+
+admin.site.register(Email)
 admin.site.register(Isp)
--- telecom/models.py
+++ telecom/models.py
@@ -1,14 +1,22 @@
 from django.db import models
 
 
+class Email(models.Model):
+
+    email = models.EmailField(max_length=127)
+
+    def __str__(self):
+        return self.email
+
+
 class Isp(models.Model):
 
     name = models.CharField(max_length=63)
     cname = models.CharField(max_length=63)
-    upstream_as = models.CharField(max_length=63)
+    upstream_as = models.IntegerField()
     primary_contact = models.CharField(max_length=63)
     email = models.EmailField(max_length=127)
-    phone_number = models.CharField(max_length=63)
-    cellphone_number = models.CharField(max_length=63)
     cc = models.CharField(max_length=63)
+    telephone = models.CharField(max_length=63)
+    cellphone = models.CharField(max_length=63)
     remark = models.CharField(max_length=63)
