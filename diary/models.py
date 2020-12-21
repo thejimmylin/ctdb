dc081bd0c924f4921dbeda1ed2d65290055d60b8
Jimmy Lin <64210965+j3ygithub@users.noreply.github.com>
2020-12-21 17:54:10 +0800

    Update models.py

--- diary/models.py
+++ diary/models.py
@@ -3,6 +3,9 @@ from django.db import models
 
 class Person(models.Model):
 
+    department = models.CharField(max_length=63, blank=True)
+    job_title = models.CharField(max_length=63, blank=True)
+    staff_code = models.CharField(max_length=63, blank=True)
     name = models.CharField(max_length=63)
 
     def __str__(self):
