b5808df5166aaf4757ffbbd36d3f76874698af29
Jimmy Lin <b00502013@gmail.com>
2021-01-10 21:25:02 +0800

    Update brand img and title

--- ctdb/urls.py
+++ ctdb/urls.py
@@ -1,13 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.contrib.auth.views import LoginView
 from django.conf import settings
-from accounts.forms import LoginForm
+from django.views.generic import RedirectView
+
 
 urlpatterns = [
     path('i18n/', include('django.conf.urls.i18n')),
     path('accounts/', include('accounts.urls')),
-    path('', LoginView.as_view(form_class=LoginForm, template_name='index.html'), name='index'),
+    path('', RedirectView.as_view(url='accounts/login/'), name='index'),
 ]
 
 if settings.DEBUG:
