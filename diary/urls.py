9a15258ba3b3d5b28b52a57f948ac79a671fbd17
Jimmy Lin <b00502013@gmail.com>
2021-01-11 03:26:56 +0800

    Add a create view

--- diary/urls.py
+++ diary/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import DiaryListView
+from .views import DiaryListView, DiaryCreateView
+
+app_name = 'diary'
 
 urlpatterns = [
-    path('', DiaryListView.as_view(), name='diary-list'),
+    path('', DiaryListView.as_view(), name='list'),
+    path('add/', DiaryCreateView.as_view(), name='add'),
 ]
--- diary/views.py
+++ diary/views.py
@@ -1,7 +1,19 @@
 from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
 from .models import Diary
 
-class DiaryListView(ListView):
-
+class DiaryListView(LoginRequiredMixin, ListView):
+    login_url = reverse_lazy('accounts:login')
     model = Diary
-    paginate_by = 1
+    paginate_by = 5
+
+    def get_queryset(self):
+        queryset = super().get_queryset().filter(created_by=self.request.user)
+        return queryset
+
+
+class DiaryCreateView(CreateView):
+    model = Diary
+    fields = ['todo']
