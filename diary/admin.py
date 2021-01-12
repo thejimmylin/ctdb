8e28d3c0f5a96cdb7230916dda18fccd3af9cda6
Jimmy Lin <64210965+j3ygithub@users.noreply.github.com>
2021-01-12 14:00:54 +0800

    Update po

--- diary/admin.py
+++ diary/admin.py
@@ -2,4 +2,9 @@ from django.contrib import admin
 from .models import Diary
 
 
-admin.site.register(Diary)
+class DiaryAdmin(admin.ModelAdmin):
+
+    list_display = ['date', 'todo', 'daily_record', 'daily_check']
+
+
+admin.site.register(Diary, DiaryAdmin)
