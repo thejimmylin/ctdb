7984beb275f2d2cdc6d9ab782e4ac2952093355f
Jimmy Lin <64210965+j3ygithub@users.noreply.github.com>
2021-01-21 11:31:40 +0800

    Update notifydiaryusers.py
    Now we can send read data with django ORM and send email with django mangement command

--- diary/management/commands/notifydiaryusers.py
+++ diary/management/commands/notifydiaryusers.py
@@ -1,4 +1,7 @@
 from django.core.management.base import BaseCommand
+from django.conf import settings
+from django.core.mail import send_mail
+from diary.models import Diary
 
 
 class Command(BaseCommand):
@@ -9,6 +12,16 @@ class Command(BaseCommand):
         To be implemented.
         """
         print('Handling notifying users of the diary app.')
+        print(Diary.objects.all())
+        subject = 'Hello world.'
+        message = 'hello, how are you?'
+        send_mail(
+            subject=subject,
+            message=message,
+            from_email=settings.DEFAULT_FROM_EMAIL,
+            recipient_list=['b00502013@gmail.com'],
+            fail_silently=False,
+        )
         print('..')
         print('..')
         print('Finished!')
