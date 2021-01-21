37adda6c526e763446d3669c0c00f4138ec6a89d
Jimmy Lin <64210965+j3ygithub@users.noreply.github.com>
2021-01-21 11:48:51 +0800

    Update notifydiaryusers.py

--- diary/management/commands/notifydiaryusers.py
+++ diary/management/commands/notifydiaryusers.py
@@ -1,5 +1,6 @@
 from django.core.management.base import BaseCommand
 from django.conf import settings
+from django.contrib.auth import get_user_model
 from django.core.mail import send_mail
 from diary.models import Diary
 
@@ -8,20 +9,23 @@ class Command(BaseCommand):
     help = 'Commands of notifying users of the diary app.'
 
     def handle(self, *args, **options):
-        """
-        To be implemented.
-        """
-        print('Handling notifying users of the diary app.')
-        print(Diary.objects.all())
-        subject = 'Hello world.'
-        message = 'hello, how are you?'
-        send_mail(
-            subject=subject,
-            message=message,
-            from_email=settings.DEFAULT_FROM_EMAIL,
-            recipient_list=['b00502013@gmail.com'],
-            fail_silently=False,
-        )
-        print('..')
-        print('..')
-        print('Finished!')
+        User = get_user_model()
+        users = User.objects.all()
+        for user in users:
+            print(f'username = "{user.username}", email = "{user.email}"')
+            diarys = Diary.objects.filter(created_by=user)
+            for diary in diarys:
+                print('Diarys:\n')
+                print(f'id = {diary.id}, daily_record = "{diary.daily_record}"')
+                """
+                Send Email..
+                """
+                # subject = 'foo'
+                # message = 'bar'
+                # send_mail(
+                #     subject=subject,
+                #     message=message,
+                #     from_email=settings.DEFAULT_FROM_EMAIL,
+                #     recipient_list=['b00502013@gmail.com'],
+                #     fail_silently=False,
+                # )
