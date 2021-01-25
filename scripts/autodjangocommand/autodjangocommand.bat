06c7510537ab71054e4a2c2520eadd964e2f63b4
Jimmy Lin <64210965+j3ygithub@users.noreply.github.com>
2021-01-25 16:19:18 +0800

    Update django schedule

--- scripts/autodjangocommand/autodjangocommand.bat
+++ scripts/autodjangocommand/autodjangocommand.bat
@@ -1,4 +1,3 @@
 call C:\Users\jimmy_lin\repos\ctdb\.venv\Scripts\activate
-python C:\Users\jimmy_lin\repos\ctdb\scripts\autodjangocommand
-automysqldump.py
+python C:\Users\jimmy_lin\repos\ctdb\scripts\autodjangocommand\autodjangocommand.py
 pause
--- scripts/autodjangocommand/autodjangocommand.py
+++ scripts/autodjangocommand/autodjangocommand.py
@@ -3,18 +3,17 @@ import time
 import schedule
 
 
-# to be modified
-def dump_job(host, user, password, db_name):
+def calld_django_send_diary_user_email():
     time_string = time.strftime('%Y%m%d%H%M%S')
     print(time_string)
     print('start..')
-    folder_name = db_name
-    file_name = db_name + '.' + time_string + '.sql'
-    cmd = f'mysqldump -h {host} -u {user} --password={password} {db_name} > {folder_name}\\{file_name}'
+    cmd = r'call C:\Users\jimmy_lin\repos\ctdb\.venv\Scripts\activate'
+    os.system(cmd)
+    cmd = r'python C:\Users\jimmy_lin\repos\ctdb\manage.py senddiaryuseremail'
     os.system(cmd)
 
 
-schedule.every().day.at('13:05').do(dump_job, host='10.210.201.12', user='admin', password='20180105', db_name='mssqlj3ydb')
+schedule.every().day.at('16:20').do(calld_django_send_diary_user_email)
 
 while True:
     schedule.run_pending()
