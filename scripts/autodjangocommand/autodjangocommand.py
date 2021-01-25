f41775e425bca6a9d894601ea93c322020cda276
Jimmy Lin <64210965+j3ygithub@users.noreply.github.com>
2021-01-25 16:29:37 +0800

    Update schedule codes

--- scripts/autodjangocommand/autodjangocommand.py
+++ scripts/autodjangocommand/autodjangocommand.py
@@ -5,15 +5,15 @@ import schedule
 
 def calld_django_send_diary_user_email():
     time_string = time.strftime('%Y%m%d%H%M%S')
-    print(time_string)
-    print('start..')
+    print(time_string)  # This would not show on windows 10 cmd, to be fixed.
+    print('start..')  # This would not show on windows 10 cmd, to be fixed.
     cmd = r'call C:\Users\jimmy_lin\repos\ctdb\.venv\Scripts\activate'
     os.system(cmd)
-    cmd = r'python C:\Users\jimmy_lin\repos\ctdb\manage.py senddiaryuseremail'
+    cmd = r'call python C:\Users\jimmy_lin\repos\ctdb\manage.py senddiaryuseremail'
     os.system(cmd)
 
 
-schedule.every().day.at('16:20').do(calld_django_send_diary_user_email)
+schedule.every().day.at('16:29').do(calld_django_send_diary_user_email)
 
 while True:
     schedule.run_pending()
