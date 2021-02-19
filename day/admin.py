a6c6703b6025f2aba3180a3836be55399f15e223
Jimmy Lin <64210965+j3ygithub@users.noreply.github.com>
2021-02-19 17:00:41 +0800

    Update admin.py

--- day/admin.py
+++ day/admin.py
@@ -1,16 +1,11 @@
 from django.contrib import admin
-from django.utils.translation import gettext_lazy as _
 
 from .models import Day
 
 
 class DayAdmin(admin.ModelAdmin):
 
-    def href(self, obj):
-        return _('See more..')
-
-    list_display_links = ['href']
-    list_display = ['date', 'is_holiday', 'href']
+    list_display = ['id', 'date', 'is_holiday', ]
     list_editable = ['date', 'is_holiday', ]
 
 
