ecd19fa52ea110e1b8ae62a5cd1f4b6e0a63dffd
Jimmy Lin <64210965+j3ygithub@users.noreply.github.com>
2021-02-23 14:42:11 +0800

    Log version1.0

--- log/urls.py
+++ log/urls.py
@@ -5,5 +5,5 @@ from .views import diary_log_list
 app_name = 'log'
 
 urlpatterns = [
-    path('diary-logs/', diary_log_list, name='list'),
+    path('diary/diaries/', diary_log_list, name='diary_log_list'),
 ]
--- log/views.py
+++ log/views.py
@@ -5,6 +5,10 @@ from django.core.paginator import Paginator
 
 from .models import Log
 from accounts.views import get_role
+from django.contrib.auth import get_user_model
+
+
+User = get_user_model()
 
 
 def diary_log_list(request):
@@ -18,12 +22,26 @@ def diary_log_list(request):
     is_supervisor = user.groups.filter(name='Supervisors').exists()
     diary_logs = Log.objects.all().order_by('-created_at').values()
     available_diary_logs = []
-    for diary_log in diary_logs:
+    if is_supervisor:
+        for diary_log in diary_logs:
+            data_as_dict = json.loads(diary_log['data'])
+            try:
+                created_by = User.objects.get(id=data_as_dict.get('created_by'))
+            except User.DoesNotExist as e:
+                print(e)
+            else:
+                dep_names = [dep.name for dep in created_by.profile.department.all()]
+                if role in dep_names:
+                    available_diary_logs.append(diary_log)
+    else:
+        for diary_log in diary_logs:
+            data_as_dict = json.loads(diary_log['data'])
+            if data_as_dict.get('created_by') == request.user.id:
+                available_diary_logs.append(diary_log)
+    for diary_log in available_diary_logs:
         data_as_dict = json.loads(diary_log['data'])
-        if data_as_dict.get('created_by', '') == request.user.id:
-            diary_log['data_as_dict'] = data_as_dict
-            available_diary_logs.append(diary_log)
-    paginator = Paginator(diary_logs, paginate_by)
+        diary_log['data_as_dict'] = data_as_dict
+    paginator = Paginator(available_diary_logs, paginate_by)
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
     # temp solution for "all pages" view.
@@ -31,6 +49,6 @@ def diary_log_list(request):
         is_paginated = False
     else:
         is_paginated = use_pagination and page_obj.has_other_pages()
-    object_list = page_obj if is_paginated else diary_logs
+    object_list = page_obj if is_paginated else available_diary_logs
     context = {'page_obj': page_obj, 'object_list': object_list, 'is_paginated': is_paginated, 'is_supervisor': is_supervisor, }
     return render(request, template_name, context)
