33d64d422ac511fdffe764c400345a0d16215a88
Jimmy Lin <64210965+j3ygithub@users.noreply.github.com>
2021-02-25 11:06:32 +0800

    customize ckeditor toolbar

--- diary/static/diary/js/diary_form.js
+++ diary/static/diary/js/diary_form.js
@@ -1,4 +1,6 @@
 const textareas = document.querySelectorAll('textarea.ckeditor4');
 for (const textarea of textareas) {
-  CKEDITOR.replace(textarea.id);
+    CKEDITOR.replace(textarea.id, {
+        customConfig: 'custom_config.js'
+    });
 }
