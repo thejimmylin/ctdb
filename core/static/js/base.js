f4150fce2661c1759146d5851657c96e3bf8cb69
Jimmy Lin <64210965+j3ygithub@users.noreply.github.com>
2021-03-04 17:18:47 +0800

    Rewrite i18n/setlang javascript

--- core/static/js/base.js
+++ core/static/js/base.js
@@ -1,24 +1,30 @@
-$(document).ready(function () {
-    $("a.set-language-item").on("click", function () {
-        var language = $(this).text();
-        $.ajax({
-            type: "POST",
-            url: url,
-            data: {
-                csrfmiddlewaretoken: csrfmiddlewaretoken,
-                language: language,
-            },
-            success: function () {
-                location.reload();
-            }
-        });
+// Django i18n/setlang
+const setLanguage = function (language) {
+    $.ajax({
+        type: "POST",
+        url: url,
+        data: {
+            csrfmiddlewaretoken: csrfmiddlewaretoken,
+            language: language,
+        },
+        success: function () {
+            location.reload();
+        },
     });
+};
+document.addEventListener("DOMContentLoaded", function () {
+    const btns = document.querySelectorAll("a.set-language-item");
+    for (const btn of btns) {
+        btn.addEventListener("click", function (event) {
+            setLanguage(event.currentTarget.textContent);
+        });
+    }
 });
 
 // scroll-sensitive things
 $(function () {
     $(document).scroll(function () {
-        var $scrollSensitive = $(".scroll-sensitive");
-        $scrollSensitive.toggleClass('scrolled', $(this).scrollTop() > $scrollSensitive.height());
+        var scrollSensitive = $(".scroll-sensitive");
+        scrollSensitive.toggleClass('scrolled', $(this).scrollTop() > scrollSensitive.height());
     });
 });
