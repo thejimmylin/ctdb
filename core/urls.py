a6ed083f0a77d6d579c64e5411c9fbda07cf6d83
Jimmy Lin <64210965+j3ygithub@users.noreply.github.com>
2021-03-04 15:06:12 +0800

    fix django template block-no-close bug

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import about
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('about/', about, name='about'),
+    path('', RedirectView.as_view(url='diary/diaries/'), name='index'),
 ]
--- ctdb/urls.py
+++ ctdb/urls.py
@@ -1,13 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.views.generic import RedirectView
 
 urlpatterns = [
     path('i18n/', include('django.conf.urls.i18n')),
     path('accounts/', include('accounts.urls')),
     path('diary/', include('diary.urls')),
     path('log/', include('log.urls')),
-    path('', include('core.urls')),
-    path('', RedirectView.as_view(url='diary/diaries/'), name='index'),
     path('admin/', admin.site.urls),
+    path('', include('core.urls')),
 ]
