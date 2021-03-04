49967425e46298d99942bfffdc692516a5ef873a
Jimmy Lin <64210965+j3ygithub@users.noreply.github.com>
2021-03-04 14:59:06 +0800

    Add a empty about page

--- ctdb/urls.py
+++ ctdb/urls.py
@@ -2,12 +2,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import RedirectView
 
-
 urlpatterns = [
     path('i18n/', include('django.conf.urls.i18n')),
     path('accounts/', include('accounts.urls')),
     path('diary/', include('diary.urls')),
     path('log/', include('log.urls')),
+    path('', include('core.urls')),
     path('', RedirectView.as_view(url='diary/diaries/'), name='index'),
     path('admin/', admin.site.urls),
 ]
