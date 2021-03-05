b078ff4d331f2e474f85623256b29d4d534ad952
Jimmy Lin <64210965+j3ygithub@users.noreply.github.com>
2021-03-05 13:29:59 +0800

    Update base.js
    Rewrite i18n/setlang

--- core/static/js/base.js
+++ core/static/js/base.js
@@ -1,45 +1,29 @@
 // Function definition
 const setLanguage = function (language) {
-    $.ajax({
-        type: "POST",
-        url: url,
-        data: {
-            csrfmiddlewaretoken: csrfmiddlewaretoken,
-            language: language,
-        },
-        success: function () {
-            location.reload();
-        },
+  fetch(url, {
+      method: "POST",
+      body: new URLSearchParams({
+        "csrfmiddlewaretoken": csrfmiddlewaretoken,
+        "language": language,
+      }),
+    })
+    .then(function () {
+      location.reload();
     });
 };
 
-// Working..
-
-// const setLanguage = function (language) {
-//     fetch(url, {
-//         method: "POST",
-//         body: JSON.stringify({
-//             csrfmiddlewaretoken: csrfmiddlewaretoken,
-//             language: language,
-//         }),
-//         headers: new Headers({
-//             "X-CSRFToken": csrfmiddlewaretoken,
-//         })
-//     }).then(location.reload())
-// };
-
 // Load these codes after DOM loaded
 document.addEventListener("DOMContentLoaded", function () {
-    // Django i18n/setlang
-    const btns = document.querySelectorAll("a.set-language-item");
-    for (const btn of btns) {
-        btn.addEventListener("click", function (event) {
-            setLanguage(event.currentTarget.textContent);
-        });
-    }
-    // Scroll-sensitive things
-    document.addEventListener("scroll", function () {
-        let scrollSensitive = document.querySelector(".scroll-sensitive");
-        scrollSensitive.classList.toggle('scrolled', window.scrollY > 0);
-    })
+  // Django i18n/setlang
+  const btns = document.querySelectorAll("a.set-language-item");
+  for (const btn of btns) {
+    btn.addEventListener("click", function (event) {
+      setLanguage(event.currentTarget.textContent);
+    });
+  }
+  // Scroll-sensitive things
+  document.addEventListener("scroll", function () {
+    let scrollSensitive = document.querySelector(".scroll-sensitive");
+    scrollSensitive.classList.toggle('scrolled', window.scrollY > 0);
+  })
 });
