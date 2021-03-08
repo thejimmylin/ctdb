4b503ca4095644fb11b9295fa0e0cb09cea8ff28
Jimmy Lin <64210965+j3ygithub@users.noreply.github.com>
2021-03-08 16:45:35 +0800

    Update indentation

--- core/static/js/base.js
+++ core/static/js/base.js
@@ -1,29 +1,29 @@
 // Function definition
 const setLanguage = function (language) {
-  fetch(url, {
-      method: "POST",
-      body: new URLSearchParams({
-        "csrfmiddlewaretoken": csrfmiddlewaretoken,
-        "language": language,
-      }),
-    })
-    .then(function () {
-      location.reload();
-    });
+    fetch(url, {
+            method: "POST",
+            body: new URLSearchParams({
+                "csrfmiddlewaretoken": csrfmiddlewaretoken,
+                "language": language,
+            }),
+        })
+        .then(function () {
+            location.reload();
+        });
 };
 
 // Load these codes after DOM loaded
 document.addEventListener("DOMContentLoaded", function () {
-  // Django i18n/setlang
-  const btns = document.querySelectorAll("a.set-language-item");
-  for (const btn of btns) {
-    btn.addEventListener("click", function (event) {
-      setLanguage(event.currentTarget.textContent);
-    });
-  }
-  // Scroll-sensitive things
-  document.addEventListener("scroll", function () {
-    let scrollSensitive = document.querySelector(".scroll-sensitive");
-    scrollSensitive.classList.toggle('scrolled', window.scrollY > 0);
-  })
+    // Django i18n/setlang
+    const btns = document.querySelectorAll("a.set-language-item");
+    for (const btn of btns) {
+        btn.addEventListener("click", function (event) {
+            setLanguage(event.currentTarget.textContent);
+        });
+    }
+    // Scroll-sensitive things
+    document.addEventListener("scroll", function () {
+        let scrollSensitive = document.querySelector(".scroll-sensitive");
+        scrollSensitive.classList.toggle('scrolled', window.scrollY > 0);
+    })
 });
