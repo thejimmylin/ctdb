e14992c9467c80526393e257b95bb25b2b9d606b
Jimmy Lin <64210965+j3ygithub@users.noreply.github.com>
2021-03-09 11:52:12 +0800

    Update views.py

--- log/views.py
+++ log/views.py
@@ -11,13 +11,6 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 
-def filter_jsonfield(qs, jsonfield_name, *args, **kwargs):
-    """
-    Filtering..
-    """
-    return qs
-
-
 def diary_log_list(request):
     model = Log
     use_pagination = True
@@ -29,34 +22,24 @@ def diary_log_list(request):
     role = get_role(request)  # NOQA, to be used
     is_supervisor = request.user.groups.filter(name='Supervisors').exists()
     qs = model.objects.all().order_by(*order_by).values()
-    """
-    To be refactored.
-    """
     object_list = []
     if is_supervisor:
         for instance in qs:
             data_as_dict = json.loads(instance['data'])
-            try:
-                created_by = User.objects.get(id=data_as_dict.get('created_by'))
-            except User.DoesNotExist as e:
-                print(e)
-            else:
-                dep_names = [dep.name for dep in created_by.profile.department.all()]
-                if role in dep_names:
-                    object_list.append(instance)
+            created_by = User.objects.filter(id=data_as_dict.get('created_by')).first()
+            if not created_by:
+                continue
+            if role in [dep.name for dep in created_by.profile.department.all()]:
+                object_list.append(instance)
     else:
         for instance in qs:
             data_as_dict = json.loads(instance['data'])
             if data_as_dict.get('created_by') == request.user.id:
                 object_list.append(instance)
     map(lambda obj: instance.update({'data_as_dict': json.loads(obj['data'])}), object_list)
-    """
-    To be refactored.
-    """
     paginator = Paginator(object_list, paginate_by)
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
-    # temp solution for "all pages" view.
     if str(page_number).lower() == 'all':
         is_paginated = False
     else:
