de34b76640c432406a5fcd7b3709ade52816002a
Jimmy Lin <64210965+j3ygithub@users.noreply.github.com>
2021-03-09 11:17:39 +0800

    Update models.py

--- log/models.py
+++ log/models.py
@@ -3,7 +3,6 @@ from django.utils.translation import gettext_lazy as _
 
 
 class Log(models.Model):
-
     action = models.CharField(verbose_name=_('Action'), max_length=63)
     app_label = models.CharField(verbose_name=_('APP label'), max_length=63)
     model_name = models.CharField(verbose_name=_('Model name'), max_length=63)
