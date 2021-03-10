a94a2a53a43dd347a7fcad6399be4d29a5357ad3
Jimmy Lin <b00502013@gmail.com>
2021-03-10 12:15:34 +0800

    Update 3 templatetag

--- telecom/templatetags/configs_parser.py
+++ telecom/templatetags/configs_parser.py
@@ -1,6 +1,7 @@
 from django import template
 from django.utils.safestring import mark_safe
 import json
+import pandas
 
 register = template.Library()
 
@@ -14,3 +15,42 @@ def parse_configs_as_html(value):
             s = f'{key}: {value}'
             lines.append(s)
     return mark_safe('\n'.join(lines))
+
+
+def parse(configs):
+    if isinstance(configs, dict):
+        sorted_keys = sorted(configs)
+        if len(configs) > 1:
+            first_key = sorted_keys[0]
+            value = configs.pop(first_key)
+            return '\n' + str(first_key) + ': ' + parse(value) + parse(configs)
+        if len(configs) == 1:
+            first_key = sorted_keys[0]
+            value = configs.pop(first_key)
+            return '\n' + str(first_key) + ': ' + parse(value)
+        else:
+            return ''
+    if isinstance(configs, list):
+        if len(configs) > 1:
+            return parse(configs[0]) + parse(configs[1:])
+        elif len(configs) == 1:
+            return parse(configs[0])
+        else:
+            return ''
+    elif isinstance(configs, str):
+        return configs + ', '
+    else:
+        return ''
+
+
+@register.filter()
+def parse_configs_as_html2(value):
+    configs = json.loads(value)
+    return parse(configs)
+
+
+@register.filter()
+def parse_configs_as_html3(value):
+    df = pandas.read_json(value)
+    html = df.to_html(index=False, justify='left')
+    return mark_safe(html)
