236042bdba066d59663b47b1874dcc007a59dbf6
Jimmy Lin <b00502013@gmail.com>
2021-03-14 16:07:13 +0800

    update checkbox fit template

--- reminder/models.py
+++ reminder/models.py
@@ -3,6 +3,7 @@ from django.db import models
 from django.utils.translation import gettext_lazy as _
 from core.utils import today
 from django.urls import reverse
+from core.validators import validate_semicolon_seperated_email_string
 
 
 class Reminder(models.Model):
@@ -18,13 +19,18 @@ class Reminder(models.Model):
         ('%(n)sth of every month' % {'n': n}, _('%(n)sth of every month') % {'n': n})
         for n in range(4, 32)
     ]
+    enabled = models.BooleanField(verbose_name=_('Enabled'), default=True)
     event = models.CharField(verbose_name=_('Event'), max_length=63)
     start_date = models.DateField(verbose_name=_('Start date'), default=today)
     stop_date = models.DateField(verbose_name=_('Stop date'), default=today)
     period = models.CharField(verbose_name=_('Period'), max_length=63, choices=PERIOD, default='on weekdays')
     email_subject = models.CharField(verbose_name=_('Email subject'), max_length=63)
     email_content = models.TextField(verbose_name=_('Email content'), blank=True)
-    recipients = models.TextField(verbose_name=_('Recipients'), help_text=_('Use ";" to seperate multiple recipient.'))
+    recipients = models.TextField(
+        verbose_name=_('Recipients'),
+        help_text=_('Use ";" to seperate multiple recipient.'),
+        validators=[validate_semicolon_seperated_email_string],
+    )
     created_by = models.ForeignKey(verbose_name=_('Created by'), to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
     def get_create_url(self):
--- telecom/models.py
+++ telecom/models.py
@@ -2,8 +2,8 @@ from django.conf import settings
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 from django.urls import reverse
-from .validators import validate_comma_separated_prefix_list_string
-from .validators import validate_semicolon_seperated_email_string
+from core.validators import validate_comma_separated_prefix_list_string
+from core.validators import validate_semicolon_seperated_email_string
 
 
 class Isp(models.Model):
