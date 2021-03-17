7217c606ae77f49405b3181d60608a8c4fb16094
Jimmy Lin <64210965+j3ygithub@users.noreply.github.com>
2021-03-17 10:04:08 +0800

    Update utils.py

--- core/utils.py
+++ core/utils.py
@@ -1,5 +1,14 @@
+from datetime import timedelta
 from django.utils import timezone
+
+
+def now():
+    return timezone.localtime(timezone.now())
 
 
 def today():
     return timezone.localtime(timezone.now()).date()
+
+
+def tomorrow():
+    return timezone.localtime(timezone.now() + timedelta(days=1)).date()
