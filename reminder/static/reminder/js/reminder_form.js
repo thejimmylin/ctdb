9b4eeffbcfabafe3965909095a7a7902ef3195d3
Jimmy Lin <64210965+j3ygithub@users.noreply.github.com>
2021-03-19 11:50:03 +0800

    Update reminder_form.js
    // INIT
    formRowSpecifiedDates.style.display = "none";

--- reminder/static/reminder/js/reminder_form.js
+++ reminder/static/reminder/js/reminder_form.js
@@ -10,7 +10,7 @@ document.addEventListener("DOMContentLoaded", function() {
     const formRowEndAt = document.querySelector("#id_form_row_end_at");
     const formRowSpecifiedDates = document.querySelector("#id_form_row_specified_dates");
     // INIT
-    formRowSpecifiedDates.style.display = "";
+    formRowSpecifiedDates.style.display = "none";
     // Event
     inputPolicy.addEventListener("change", function(event) {
         if (inputPolicy.value === 'specified dates') {
