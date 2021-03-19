5774d624c123434b9996c2ec65ec2079811ada07
Jimmy Lin <b00502013@gmail.com>
2021-03-19 23:12:00 +0800

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -1,11 +1,10 @@
 from django.urls import include, path
 from django.views.generic import RedirectView
 
-from .views import http404, news
+from .views import news
 
 urlpatterns = [
     path('news/', news, name='news'),
     path('i18n/', include('django.conf.urls.i18n')),
     path('', RedirectView.as_view(url='/news/'), name='index'),
-    path('<path:path>', http404, name='http404'),
 ]
