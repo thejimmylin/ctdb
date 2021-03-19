b3e102fe0083d4379eb3e148c677fa0fd944a9db
Jimmy Lin <b00502013@gmail.com>
2021-03-19 23:12:23 +0800

    Update views.py

--- core/views.py
+++ core/views.py
@@ -6,8 +6,3 @@ def news(request):
     if not request.user.is_authenticated:
         return redirect(reverse("accounts:login") + '?next=' + request.get_full_path())
     return render(request, 'news.html')
-
-
-def http404(request, path):
-    context = {'path': path}
-    return render(request, 'http404.html', context)
