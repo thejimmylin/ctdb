a5ef5cf2edeb8985af73ff7f7342fb3bf663d990
Jimmy Lin <b00502013@gmail.com>
2021-03-21 13:00:06 +0800

    Update views.py

--- diary/views.py
+++ diary/views.py
@@ -1,8 +1,9 @@
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
+from django.http.response import Http404
 from django.shortcuts import get_object_or_404, redirect, render
 from django.urls import reverse
-from django.http.response import Http404
+
 from core.decorators import permission_required
 
 from .forms import DiaryModelForm
