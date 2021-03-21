4312a292b27dcc548f29224a8ea78aeb20b17949
Jimmy Lin <b00502013@gmail.com>
2021-03-21 13:34:38 +0800

    Update utils.py

--- core/utils.py
+++ core/utils.py
@@ -19,8 +19,3 @@ def remove_unnecessary_seperator(s, seperator):
     if s.recipients[-1:] == seperator:
         return s.recipients[:-1]
     return s
-
-
-def get_clean_objs(s, seperator):
-    s = remove_unnecessary_seperator(s, seperator)
-    return map(str.strip, s.split(seperator))
