5a574cb5d877c0ce6b58ca4f71a2935630d8743a
Jimmy Lin <64210965+j3ygithub@users.noreply.github.com>
2021-03-22 11:48:07 +0800

    use templatetags on list view dorpdown actions filter

--- core/templatetags/custom_templatetags.py
+++ core/templatetags/custom_templatetags.py
@@ -1,5 +1,7 @@
 from django import template
 from django.utils.safestring import mark_safe
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.auth.models import Permission
 
 register = template.Library()
 
@@ -17,3 +19,21 @@ def verbose_name(value):
 @register.filter()
 def verbose_name_plural(value):
     return value._meta.verbose_name_plural
+
+
+@register.filter()
+def has_create_perm(value, user):
+    s = f'{value._meta.app_label}.add_{value._meta.model_name}'
+    return user.has_perm(s)
+
+
+@register.filter()
+def has_update_perm(value, user):
+    s = f'{value._meta.app_label}.change_{value._meta.model_name}'
+    return user.has_perm(s)
+
+
+@register.filter()
+def has_delete_perm(value, user):
+    s = f'{value._meta.app_label}.delete_{value._meta.model_name}'
+    return user.has_perm(s)
