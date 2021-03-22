df91ba68fd65ca4f84dc8495ba0206f0e219f8cd
Jimmy Lin <64210965+j3ygithub@users.noreply.github.com>
2021-03-22 10:31:34 +0800

    Create admin.py
    admin.site.register(Permission)
    admin.site.register(ContentType)

---
+++ core/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from django.contrib.auth.models import Permission
+from django.contrib.contenttypes.models import ContentType
+
+admin.site.register(Permission)
+admin.site.register(ContentType)
