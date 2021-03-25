932b249b81b3593686dd7daa07d0b0726447b157
Jimmy Lin <64210965+j3ygithub@users.noreply.github.com>
2021-03-25 18:04:17 +0800

    update news_list

--- core/views.py
+++ core/views.py
@@ -1,10 +1,29 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from django.core.paginator import Paginator
 from core.decorators import permission_required
 from django.http.response import Http404
+
+from .models import News
 
 
 @login_required
 @permission_required('new.view_new', raise_exception=True, exception=Http404)
 def news_list(request):
-    return render(request, 'news.html')
+    model = News
+    paginate_by = 5
+    template_name = 'news_list.html'
+    is_supervisor = True
+    qs = News.objects.all()
+    page_number = request.GET.get('page', '')
+    paginator = Paginator(qs, paginate_by)
+    page_obj = paginator.get_page(page_number)
+    is_paginated = page_number.lower() != 'all' and page_obj.has_other_pages()
+    context = {
+        'model': model,
+        'page_obj': page_obj,
+        'object_list': page_obj if is_paginated else qs,
+        'is_paginated': is_paginated,
+        'is_supervisor': is_supervisor,
+    }
+    return render(request, template_name, context)
