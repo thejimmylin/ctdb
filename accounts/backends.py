e165059ec68dd5586826fe8dab03bc38cdeb4d13
Jimmy Lin <64210965+j3ygithub@users.noreply.github.com>
2021-03-25 11:42:48 +0800

    Update backends.py

--- accounts/backends.py
+++ accounts/backends.py
@@ -31,9 +31,27 @@ class AuthWithUsernameOrEmailBackend(ModelBackend):
         base_perm = super().has_perm(user_obj, perm, obj=None)
         if obj is None:
             return base_perm
-        return base_perm and self.has_obj_perm(user_obj, perm, obj=obj)
+        return base_perm and self._has_obj_perm(user_obj, perm, obj=obj)
 
-    def has_obj_perm(self, user_obj, perm, obj):
+    def _has_obj_perm(self, user_obj, perm, obj):
+        """
+        The `obj` should be the same model as what `perm` says.
+        """
+        app_label, codename = perm.split('.')
+        action, model_name = codename.split('_')
+        if not obj._meta.model_name == model_name:
+            return False
+        return self.has_obj_perm(user_obj, action, obj)
+
+    def has_obj_perm(self, user_obj, action, obj):
+        """
+        Generally speaking, a specific object doesn't has its `add` permission.
+        """
+        actions = ('view', 'change', 'delete')
+        if action not in actions:
+            return False
+        if action == 'view':
+            return True
         if not hasattr(obj, 'created_by'):
             return False
         return obj.created_by == user_obj
