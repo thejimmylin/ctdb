9b37b589435a4b4d3dea79eaceea59699ac506f1
Jimmy Lin <b00502013@gmail.com>
2021-03-26 00:11:38 +0800

    news version 1

--- core/models.py
+++ core/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
+from django.urls import reverse
 
 
 class News(models.Model):
@@ -11,3 +12,12 @@ class News(models.Model):
         ordering = ['-id']
         verbose_name = _('New')
         verbose_name_plural = _('News')
+
+    def get_create_url(self):
+        return reverse('news_create')
+
+    def get_update_url(self):
+        return reverse('news_update', kwargs={'pk': self.pk})
+
+    def get_delete_url(self):
+        return reverse('news_delete', kwargs={'pk': self.pk})
--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,13 @@
 from django.urls import include, path
 from django.views.generic import RedirectView
 
-from .views import news_list
+from .views import news_list, news_create, news_update, news_delete
 
 urlpatterns = [
     path('news/', news_list, name='news_list'),
+    path('news/add/', news_create, name='news_create'),
+    path('news/<int:pk>/change/', news_update, name='news_update'),
+    path('news/<int:pk>/delete/', news_delete, name='news_delete'),
     path('i18n/', include('django.conf.urls.i18n')),
     path('', RedirectView.as_view(url='/news/'), name='index'),
 ]
