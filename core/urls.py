591d869b61e18009e229dc014e0e0ed4273300ae
Jimmy Lin <64210965+j3ygithub@users.noreply.github.com>
2021-03-25 17:54:53 +0800

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,10 @@
 from django.urls import include, path
 from django.views.generic import RedirectView
 
-from .views import news
+from .views import news_list
 
 urlpatterns = [
-    path('news/', news, name='news'),
+    path('news/', news_list, name='news_list'),
     path('i18n/', include('django.conf.urls.i18n')),
     path('', RedirectView.as_view(url='/news/'), name='index'),
 ]
