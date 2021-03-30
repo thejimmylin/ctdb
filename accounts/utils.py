969ff4be42cb07245b2bcdad90691fb61f6e1041
Jimmy Lin <b00502013@gmail.com>
2021-03-30 23:42:30 +0800

    a

--- accounts/utils.py
+++ accounts/utils.py
@@ -1,8 +1,5 @@
 def get_role(session, user):
-    default_role = user.profile.get_default_role()
-    if not default_role:
-        default_role = {'pk': 0, 'name': ''}
-    role = session.get('role', default_role)
-    if role not in user.get_available_roles():
-        return default_role
-    return role
+    role = session.get('role')
+    if role in user.get_available_roles():
+        return role
+    return {}
--- diary/querys.py
+++ diary/querys.py
@@ -4,18 +4,14 @@ from django.db.models import Q
 from accounts.utils import get_role
 
 
-def get_diary_query(user, *arg, **kwargs):
-    if 'session' not in kwargs:
-        return Q(created_by=user)
-    session = kwargs.pop('session')
-    get_role(user=user, session=session)
-    role = session.get('role', {'pk': 0, 'name': ''})
-    if not role:
-        return Q(created_by=user)
-    role_pk = role['pk']
-    print(role_pk)
-    role = Group.objects.get(pk=role_pk)
+def get_diary_queryset(request):
+    role = get_role(user=request.user, session=request.session)
+    pk = role.get('pk', 0)
+    role = Group.objects.get(pk=pk)
     if role.name == 'T00 supervisor':
-        print('you can see all')
-        return Q()
-    return Q(created_by=user)
+        return Q(created_by__group__name__in=['T00', 'T01', 'T02', 'T11', 'T12', 'T21', 'T22', 'T31', 'T32'])
+    if role.name == 'T01 supervisor':
+        return Q(created_by__group__name__in=['T01', 'T11', 'T12'])
+    return Q(created_by=request.user)
+
+Q(groups__name__in=['T01', 'T11', 'T12'])
