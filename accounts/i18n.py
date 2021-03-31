861904a1ee8b1bfaac1e45bcf9af138eca418e13
Jimmy Lin <b00502013@gmail.com>
2021-04-01 01:40:02 +0800

    Update i18n.py

--- accounts/i18n.py
+++ accounts/i18n.py
@@ -5,6 +5,15 @@ They maybe appear in DB only, so they can't be detected without being writed exp
 from django.utils.translation import gettext_lazy as _
 
 I18N_NEEDED = [
+    _('T00 member'),
+    _('T01 member'),
+    _('T02 member'),
+    _('T11 member'),
+    _('T12 member'),
+    _('T21 member'),
+    _('T22 member'),
+    _('T31 member'),
+    _('T32 member'),
     _('T00 supervisor'),
     _('T01 supervisor'),
     _('T02 supervisor'),
