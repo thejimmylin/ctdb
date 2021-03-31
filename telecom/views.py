dd94c051c7e59578c2e4a6f563e52e3cd9e37979
Jimmy Lin <b00502013@gmail.com>
2021-04-01 02:19:50 +0800

    Update views.py

--- telecom/views.py
+++ telecom/views.py
@@ -4,6 +4,7 @@ from django.http.response import Http404
 from django.shortcuts import get_object_or_404, redirect, render
 from django.urls import reverse
 
+from accounts.models import get_role
 from core.decorators import permission_required
 
 from .forms import (IspGroupModelForm, IspModelForm,
@@ -11,25 +12,57 @@ from .forms import (IspGroupModelForm, IspModelForm,
 from .models import Isp, IspGroup, PrefixListUpdateTask
 
 
+def get_telecom_model_queryset(request, model):
+    """
+    The queryset of models `Isp`, `IspGroup`, `PrefixListUpdateTask` with
+    filter depending on user's role/identity/group. The views below will use
+    this as a basic queryset. This ensures that users won't accidentally see
+    or touch those they shouldn't.
+    """
+    queryset = model.objects.all()
+    role = get_role(user=request.user, session=request.session)
+    if not role:
+        return queryset.filter(created_by=request.user)
+    supervise_roles = role.groupprofile.supervise_roles.all()
+    if not supervise_roles:
+        return queryset.filter(created_by=request.user)
+    return queryset.filter(created_by__groups__in=supervise_roles).distinct()
+
+
+def get_isp_queryset(request):
+    model = Isp
+    queryset = get_telecom_model_queryset(request, model=model)
+    return queryset
+
+
+def get_ispgroup_queryset(request):
+    model = IspGroup
+    queryset = get_telecom_model_queryset(request, model=model)
+    return queryset
+
+
+def get_prefixlistupdatetask_queryset(request):
+    model = PrefixListUpdateTask
+    queryset = get_telecom_model_queryset(request, model=model)
+    return queryset
+
+
 @login_required
 @permission_required('telecom.view_isp', raise_exception=True, exception=Http404)
 def isp_list(request):
     model = Isp
+    queryset = get_isp_queryset(request)
     paginate_by = 5
     template_name = 'telecom/isp_list.html'
-    group = request.session.get('group', request.user.profile.get_default_group_name())
-    is_supervisor = True
-    qs = model.objects.filter(created_by__groups__name=group)
     page_number = request.GET.get('page', '')
-    paginator = Paginator(qs, paginate_by)
+    paginator = Paginator(queryset, paginate_by)
     page_obj = paginator.get_page(page_number)
     is_paginated = page_number.lower() != 'all' and page_obj.has_other_pages()
     context = {
         'model': model,
         'page_obj': page_obj,
-        'object_list': page_obj if is_paginated else qs,
+        'object_list': page_obj if is_paginated else queryset,
         'is_paginated': is_paginated,
-        'is_supervisor': is_supervisor,
     }
     return render(request, template_name, context)
 
@@ -59,7 +92,8 @@ def isp_create(request):
 @permission_required('telecom.change_isp', raise_exception=True, exception=Http404)
 def isp_update(request, pk):
     model = Isp
-    instance = get_object_or_404(klass=model, pk=pk, created_by=request.user)
+    queryset = get_isp_queryset(request)
+    instance = get_object_or_404(klass=queryset, pk=pk, created_by=request.user)
     form_class = IspModelForm
     success_url = reverse('telecom:isp_list')
     form_buttons = ['update']
@@ -80,7 +114,8 @@ def isp_update(request, pk):
 @permission_required('telecom.delete_isp', raise_exception=True, exception=Http404)
 def isp_delete(request, pk):
     model = Isp
-    instance = get_object_or_404(klass=model, pk=pk, created_by=request.user)
+    queryset = get_isp_queryset(request)
+    instance = get_object_or_404(klass=queryset, pk=pk, created_by=request.user)
     success_url = reverse('telecom:isp_list')
     template_name = 'telecom/isp_confirm_delete.html'
     if request.method == 'POST':
@@ -94,21 +129,18 @@ def isp_delete(request, pk):
 @permission_required('telecom.view_ispgroup', raise_exception=True, exception=Http404)
 def ispgroup_list(request):
     model = IspGroup
+    queryset = get_ispgroup_queryset(request)
     paginate_by = 5
     template_name = 'telecom/ispgroup_list.html'
-    group = request.session.get('group', request.user.profile.get_default_group_name())
-    is_supervisor = True
-    qs = model.objects.filter(created_by__groups__name=group)
     page_number = request.GET.get('page', '')
-    paginator = Paginator(qs, paginate_by)
+    paginator = Paginator(queryset, paginate_by)
     page_obj = paginator.get_page(page_number)
     is_paginated = page_number.lower() != 'all' and page_obj.has_other_pages()
     context = {
         'model': model,
         'page_obj': page_obj,
-        'object_list': page_obj if is_paginated else qs,
+        'object_list': page_obj if is_paginated else queryset,
         'is_paginated': is_paginated,
-        'is_supervisor': is_supervisor,
     }
     return render(request, template_name, context)
 
@@ -138,7 +170,8 @@ def ispgroup_create(request):
 @permission_required('telecom.change_ispgroup', raise_exception=True, exception=Http404)
 def ispgroup_update(request, pk):
     model = IspGroup
-    instance = get_object_or_404(klass=model, pk=pk, created_by=request.user)
+    queryset = get_ispgroup_queryset(request)
+    instance = get_object_or_404(klass=queryset, pk=pk, created_by=request.user)
     form_class = IspGroupModelForm
     success_url = reverse('telecom:ispgroup_list')
     form_buttons = ['update']
@@ -159,7 +192,8 @@ def ispgroup_update(request, pk):
 @permission_required('telecom.delete_ispgroup', raise_exception=True, exception=Http404)
 def ispgroup_delete(request, pk):
     model = IspGroup
-    instance = get_object_or_404(klass=model, pk=pk, created_by=request.user)
+    queryset = get_ispgroup_queryset(request)
+    instance = get_object_or_404(klass=queryset, pk=pk, created_by=request.user)
     success_url = reverse('telecom:ispgroup_list')
     template_name = 'telecom/ispgroup_confirm_delete.html'
     if request.method == 'POST':
@@ -173,21 +207,18 @@ def ispgroup_delete(request, pk):
 @permission_required('telecom.view_prefixlistupdatetask', raise_exception=True, exception=Http404)
 def prefixlistupdatetask_list(request):
     model = PrefixListUpdateTask
+    queryset = get_prefixlistupdatetask_queryset(request)
     paginate_by = 5
     template_name = 'telecom/prefixlistupdatetask_list.html'
-    group = request.session.get('group', request.user.profile.get_default_group_name())
-    is_supervisor = True
-    qs = model.objects.filter(created_by__groups__name=group)
     page_number = request.GET.get('page', '')
-    paginator = Paginator(qs, paginate_by)
+    paginator = Paginator(queryset, paginate_by)
     page_obj = paginator.get_page(page_number)
     is_paginated = page_number.lower() != 'all' and page_obj.has_other_pages()
     context = {
         'model': model,
         'page_obj': page_obj,
-        'object_list': page_obj if is_paginated else qs,
+        'object_list': page_obj if is_paginated else queryset,
         'is_paginated': is_paginated,
-        'is_supervisor': is_supervisor,
     }
     return render(request, template_name, context)
 
@@ -217,7 +248,8 @@ def prefixlistupdatetask_create(request):
 @permission_required('telecom.change_prefixlistupdatetask', raise_exception=True, exception=Http404)
 def prefixlistupdatetask_update(request, pk):
     model = PrefixListUpdateTask
-    instance = get_object_or_404(klass=model, pk=pk, created_by=request.user)
+    queryset = get_prefixlistupdatetask_queryset(request)
+    instance = get_object_or_404(klass=queryset, pk=pk, created_by=request.user)
     form_class = PrefixListUpdateTaskModelForm
     success_url = reverse('telecom:prefixlistupdatetask_list')
     form_buttons = ['update']
@@ -238,7 +270,8 @@ def prefixlistupdatetask_update(request, pk):
 @permission_required('telecom.delete_prefixlistupdatetask', raise_exception=True, exception=Http404)
 def prefixlistupdatetask_delete(request, pk):
     model = PrefixListUpdateTask
-    instance = get_object_or_404(klass=model, pk=pk, created_by=request.user)
+    queryset = get_prefixlistupdatetask_queryset(request)
+    instance = get_object_or_404(klass=queryset, pk=pk, created_by=request.user)
     success_url = reverse('telecom:prefixlistupdatetask_list')
     template_name = 'telecom/prefixlistupdatetask_confirm_delete.html'
     if request.method == 'POST':
