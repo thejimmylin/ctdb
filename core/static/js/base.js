c5b6327b4c4d1a940a082574f3b632daed79b051
Jimmy Lin <64210965+j3ygithub@users.noreply.github.com>
2021-04-01 14:40:43 +0800

    add a fetch diary api endpoint js

--- core/static/js/base.js
+++ core/static/js/base.js
@@ -1,27 +1,24 @@
 // Load these codes after DOM loaded
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", function() {
     // Function definition
-    const setLanguage = function (language) {
+    const setLanguage = function(language) {
         fetch(i18nUrl, {
-                method: "POST",
-                body: new URLSearchParams({
-                    "csrfmiddlewaretoken": csrfMiddlewareToken,
-                    "language": language,
-                }),
-            })
-            .then(function () {
-                location.reload();
-            });
+            method: "POST",
+            body: new URLSearchParams({
+                "csrfmiddlewaretoken": csrfMiddlewareToken,
+                "language": language,
+            }),
+        }).then(function() {location.reload();});
     };
     // Django i18n/setlang
     const btns = document.querySelectorAll("a.set-language-item");
     for (const btn of btns) {
-        btn.addEventListener("click", function (event) {
+        btn.addEventListener("click", function(event) {
             setLanguage(event.currentTarget.textContent);
         });
     }
     // Scroll-sensitive things
-    document.addEventListener("scroll", function () {
+    document.addEventListener("scroll", function() {
         let scrollSensitive = document.querySelector(".scroll-sensitive");
         scrollSensitive.classList.toggle('scrolled', window.scrollY > 0);
     })
--- diary/static/diary/js/diary_list.js
+++ diary/static/diary/js/diary_list.js
@@ -1,1 +1,7 @@
-let docUrl = document.URL;
+document.addEventListener("DOMContentLoaded", function() {
+    // Function definition
+    const origin = window.location.origin;
+    const urlApiDiaryList = `${origin}/api/diaries/`;
+    fetch(urlApiDiaryList).then(r => r.json()).then(console.log);
+    }
+);
