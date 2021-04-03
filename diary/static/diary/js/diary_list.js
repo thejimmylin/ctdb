6b6937e109b85c216c0d58482fd978960047f241
Jimmy Lin <b00502013@gmail.com>
2021-04-03 22:21:35 +0800

    fix toolbar css bug

--- diary/static/diary/js/diary_list.js
+++ diary/static/diary/js/diary_list.js
@@ -1,10 +1,12 @@
 document.addEventListener("DOMContentLoaded", function() {
     const pathname = window.location.pathname;
     const selectRoles = document.querySelector('#rolesSelect');
-    selectRoles.addEventListener("change", function(e) {
-        const params = e.target.value ? {"dep": e.target.value} : {};
-        const queryString = new URLSearchParams(params).toString();
-        const url = queryString ? pathname + "?" + queryString : pathname;
-        window.location.replace(url);
-    })
+    if (selectRoles) {
+        selectRoles.addEventListener("change", function(e) {
+            const params = e.target.value ? {"dep": e.target.value} : {};
+            const queryString = new URLSearchParams(params).toString();
+            const url = queryString ? pathname + "?" + queryString : pathname;
+            window.location.replace(url);
+        })
+    }
 });
