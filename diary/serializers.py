09cd6d6a95506a906904a53cd80cf08d3833b834
Jimmy Lin <b00502013@gmail.com>
2021-04-03 15:40:44 +0800

    Update serializers.py

--- diary/serializers.py
+++ diary/serializers.py
@@ -4,16 +4,18 @@ from diary.models import Diary
 
 
 class DiaryModelSerializer(serializers.ModelSerializer):
+    created_by = serializers.PrimaryKeyRelatedField(read_only=True, default=serializers.CurrentUserDefault())
+
     class Meta:
         model = Diary
-        exclude = ['created_by']
+        exclude = []
 
-    def validate(self, attrs):
-        print(dir(self))
-        print(self.instance.validate_unique())
-        print(attrs)
-        validated_data = super().validate(attrs)
-        created_by = validated_data['created_by']
-        print(created_by)
-        print(validated_data)
-        return validated_data
+    # def validate(self, attrs):
+    #     print(dir(self))
+    #     print(self.instance.validate_unique())
+    #     print(attrs)
+    #     validated_data = super().validate(attrs)
+    #     created_by = validated_data['created_by']
+    #     print(created_by)
+    #     print(validated_data)
+    #     return validated_data
