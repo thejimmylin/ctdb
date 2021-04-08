90267af0f5dd7e527b3614366984a57563ed30d3
Jimmy Lin <64210965+j3ygithub@users.noreply.github.com>
2021-04-08 15:50:03 +0800

    Update ModelTable.vue

--- src/components/ModelTable.vue
+++ src/components/ModelTable.vue
@@ -9,8 +9,20 @@
   </thead>
   <tbody>
     <tr v-for="(result, index) in results" :key="index">
-      <td class="font-normal border border-green-600 text-green-900 p-1" v-for="(field, index) in fields" :key="index">
-        {{ result }}
+      <td class="font-normal border border-green-600 text-green-900 p-1">
+        {{ result['id'] }}
+      </td>
+      <td class="font-normal border border-green-600 text-green-900 p-1">
+        {{ result['title'] }}
+      </td>
+      <td class="font-normal border border-green-600 text-green-900 p-1">
+        {{ result['content'] }}
+      </td>
+      <td class="font-normal border border-green-600 text-green-900 p-1">
+        {{ result['at'] }}
+      </td>
+      <td class="font-normal border border-green-600 text-green-900 p-1">
+        {{ result['created_by'] }}
       </td>
     </tr>
   </tbody>
