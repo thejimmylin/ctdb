56d1c26879423dd93367f12e522a1198cb83933f
Jimmy Lin <64210965+j3ygithub@users.noreply.github.com>
2021-04-12 14:27:39 +0800

    Revert "add default_auto_field = 'django.db.models.BigAutoField' to each apps"
    This reverts commit fbf051527d29eed70fe991f2c93bdaf311aaf3b6.

--- day/apps.py
+++ day/apps.py
@@ -2,5 +2,4 @@ from django.apps import AppConfig
 
 
 class DayConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
     name = 'day'
--- reminder/apps.py
+++ reminder/apps.py
@@ -2,5 +2,4 @@ from django.apps import AppConfig
 
 
 class ReminderConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
     name = 'reminder'
