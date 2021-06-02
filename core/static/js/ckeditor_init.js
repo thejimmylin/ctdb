f9fb5d602e4bda574b989a4dcd5b38ee1547aa30
Jimmy Lin <b00502013@gmail.com>
2021-06-02 15:59:36 +0800

    Update ckeditor_init.js

--- core/static/js/ckeditor_init.js
+++ core/static/js/ckeditor_init.js
@@ -1,3 +1,10 @@
+// These codes is used to change the DOMs on the page to a CKeditor.
+// CKeditor is a rich-text editor.
+// https://ckeditor.com/ckeditor-4/
+// To use a custom config: change the core\static\ckeditor\custom_config.js
+// For more detail, see the official documentation.
+// https://ckeditor.com/docs/ckeditor4/latest/guide/dev_configuration.html#configuration-loading-order
+
 document.addEventListener("DOMContentLoaded", function() {
     const textareas = document.querySelectorAll('textarea.ckeditor4');
     for (const textarea of textareas) {
